c500f7643ce2d2920fac9a1680e7110efac433ef
Andreas Schilling <andreas_schilling@gmx.net>
2015-01-02 22:10:26 +0100

    Change startup order

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
 service oracle-xe start
-/usr/sbin/sshd -D
 
 sleep 10
 
 sqlplus system/oracle @sql/setup_user.sql
+
+/usr/sbin/sshd -D
