58acd326d9c69827e27a6e70464494340225817f
Andreas Schilling <andreas_schilling@gmx.net>
2015-01-04 18:03:22 +0100

    Add some startup debug output

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
+echo Starting Oracle...
+
 sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/network/admin/listener.ora
 service oracle-xe start
 /usr/sbin/sshd -D
 
 sleep 10
 
+echo Initialize DB user...
 sqlplus system/oracle @sql/setup_user.sql
