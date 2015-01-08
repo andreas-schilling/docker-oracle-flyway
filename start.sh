191dceb483fb3ba90b566f285baa423fc9f3fa1b
Andreas Schilling <andreas_schilling@gmx.net>
2015-01-08 08:01:25 +0100

    Add Oracle wait script during DB startup

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ sed -i -E "s/HOST = [^)]+/HOST = $HOSTNAME/g" /u01/app/oracle/product/11.2.0/xe/
 service oracle-xe start
 
 echo Waiting for Oracle...
-sleep 10
+./oracle/wait_for_oracle.sh
 
 echo Initialize DB user...
 sqlplus system/oracle @sql/setup_user.sql
